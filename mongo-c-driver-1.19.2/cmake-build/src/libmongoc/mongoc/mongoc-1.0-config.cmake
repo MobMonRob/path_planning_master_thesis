e9fb2b8693187edf0f58ae1e7efc5b2d813d19d6
Nitish Garg <nitish.garg1250@gmail.com>
2021-12-01 11:31:59 +0100

    first commit

--- mongo-c-driver-1.19.2/cmake-build/src/libmongoc/mongoc/mongoc-1.0-config.cmake
+++
@@ -1,3 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(bson-1.0 1.19.2)
-include("${CMAKE_CURRENT_LIST_DIR}/mongoc-targets.cmake")
--- mongo-c-driver-1.19.2/cmake-build/src/libmongoc/src/mongoc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nitish/ws_moveit/src/mongo-c-driver-1.19.2/src/libmongoc/src/mongoc
-# Build directory: /home/nitish/ws_moveit/src/mongo-c-driver-1.19.2/cmake-build/src/libmongoc/src/mongoc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
